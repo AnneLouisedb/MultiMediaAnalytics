4cb0d926c043a2ed55190acd63d3eb0e2d40a55a
AnneLouise <71494611+AnneLouisedb@users.noreply.github.com>
2024-06-10 22:27:36 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import numpy as np
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+def main():
+    st.write("Fairness in Decision Making")
+
+  
+
+if __name__ == "__main__":
+    main()
